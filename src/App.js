2766c0293ee34310e7727ae9596085e2bbbd6675
jobeuse <iradukundajob3@gmail.com>
2022-11-03 21:49:13 +0200

    project section done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
-
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <div className="container">
       <div className="row justify-content-center">
         <Header/>
+        <Projects/>
+         <Skills/>
       </div>
   </div>
   );
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,27 @@
+
+import "./Skills.css" 
+import { motion } from "framer-motion"
+export default function Projects (){
+
+    return (
+        <section  id="Projects" className="col-lg-12 mt-5 pt-5">
+            <div className="row justify-content-start">
+                <div className="col-lg-12">
+                    <div className="row justify-content-start">
+                        <div className="col-lg-12  title justify-content-center">
+                            <h1>My Skills</h1> 
+                            <hr/>
+                        </div> 
+                        <div className="col-lg-12">
+                          <div className="row justify-content-end">
+                              <div className="col-lg-9 col-md-9 ">
+                                  Front-end
+                              </div>
+                          </div>
+                        </div>
+                    </div> 
+                </div> 
+            </div>
+        </section>
+            )
+    }
